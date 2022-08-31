c48cb165aa905854d117a7c11e75363a2c42c21c
ashishpundir9 <106304078+ashishpundir9@users.noreply.github.com>
2022-08-31 17:56:35 +0530

    Update JavaProgram.java

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,5 +1,5 @@
 public class JavaProgram{
 public static void main(String args[]){
-System.out.println("***Hello - This is first java program using Jenkins Job***");
+System.out.println("***Hello - This is first java program using Jenkins Job- Made changes***");
 }
 }
